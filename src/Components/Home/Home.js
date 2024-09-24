b90305285cd10d23a35f7edac003a795d1be4edf
alex2227 <alex2227@hotmail.com>
2024-09-24 03:13:33 -0600

    Cambiado class para uso de toda la pantalla

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -4,7 +4,7 @@ import ImagenPrincipal from '../../assets/img/Background.png';
 function Home() {
     return (
         <div>
-            <img src={ImagenPrincipal} alt="Imagen Principal" className="main-image"/>
+            <img src={ImagenPrincipal} alt="Imagen Principal" className="d-block w-100"/>
         </div>
     );
 }
