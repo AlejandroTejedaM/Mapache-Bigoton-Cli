8a56170a15ad8178babc60bee33dde60cb09f7fa
alex2227 <alex2227@hotmail.com>
2024-09-24 03:10:46 -0600

    Modificado el UsuarioContext.js para uso del tokken y estado de sesion

--- src/context/UsuarioContext.js
+++ src/context/UsuarioContext.js
@@ -1,13 +1,18 @@
-import  {createContext} from 'react';
-const UsuarioContextDefault = createContext(
-    {
-        usuarioId: 0,
-        nombre: "",
-        correo: "",
-        contrasennia: "",
-        token: "",
-        setUsuario: (usuario) => {}
-    }
-);
+import {createContext, useState} from 'react';
 
-export default UsuarioContextDefault;
+const UsuarioContext = createContext();
+
+export const UsuarioProvider = ({ children }) => {
+        const [usuario, setUsuario] = useState({
+                token: null,
+                isLogged: false,
+                // Agrega otras propiedades del usuario según sea necesario
+        });
+        return (
+            <UsuarioContext.Provider value={{ usuario, setUsuario }}>
+                    {children}
+            </UsuarioContext.Provider>
+        );
+};
+
+export default UsuarioContext;
