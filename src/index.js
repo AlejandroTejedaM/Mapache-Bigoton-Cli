0e231a2fffcf5121819c813d494f5bf6a015d1bd
alex2227 <alex2227@hotmail.com>
2024-09-24 03:13:54 -0600

    Añadido provider del usuario para toda la aplicación

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './app';
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa Bootstrap
-import './assets/css/estilos.css'; // Importa el archivo de estilos
+import './assets/css/estilos.css';
+import {UsuarioProvider} from "./context/UsuarioContext"; // Importa el archivo de estilos
 ReactDOM.render(
-    <App/>,
+    <UsuarioProvider>
+    <App/>
+    </UsuarioProvider>,
     document.getElementById('root')
 );
