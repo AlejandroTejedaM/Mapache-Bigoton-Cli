31fca89fc2c3a96695a06650b2e79d43da4ceaea
MarcoAntonioHernandez <lm682127@gmail.com>
2024-10-03 17:37:45 -0600

    Delete Mapache-Bigoton-Cli-gerard directory

--- Mapache-Bigoton-Cli-gerard/src/Components/Gallery/Gallery.js
+++
@@ -1,9 +0,0 @@
-function Gallery() {
-    return (
-        <div>
-            <h1>Galería</h1>
-        </div>
-    );
-}
-
-export default Gallery;
--- Mapache-Bigoton-Cli-gerard/src/Components/Home/Citas.js
+++
@@ -1,9 +0,0 @@
-function Citas() {
-    return (
-        <div>
-            <h1>Agenda tu cita</h1>
-        </div>
-    );
-}
-
-export default Citas;
